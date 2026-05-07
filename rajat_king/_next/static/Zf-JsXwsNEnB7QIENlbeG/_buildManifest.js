ff8b53c3dffc3f0f6b62abd0b890fec4bc47ffcc
Raj Ranjan Srivastav <raj.srivastava@kreatetechnologies.com>
2026-05-07 17:37:49 +0530

    Updates

--- rajat_king/_next/static/Zf-JsXwsNEnB7QIENlbeG/_buildManifest.js
+++ rajat_king/_next/static/Zf-JsXwsNEnB7QIENlbeG/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/rajat_king//_next/:path+",
-        "destination": "/rajat_king/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
