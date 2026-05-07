4a3e5762e66ad7fafd087294361d62ca4597f857
Raj Ranjan Srivastav <raj.srivastava@kreatetechnologies.com>
2026-05-07 20:02:47 +0530

    Updates

--- _next/static/ViQWRm2kRDpykQtSnENuN/_buildManifest.js
+++ _next/static/ViQWRm2kRDpykQtSnENuN/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/rajat_king//_next/:path+",
+        "destination": "/rajat_king/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
